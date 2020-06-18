0aecff81c6a0532fdd7af256d75f0c631b5166cc
David Boedigheimer <stealth259@gmail.com>
2020-06-18 16:00:42 -0700

    Add main body for main js.

--- src/main.js
+++ src/main.js
@@ -2,4 +2,27 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-import { nameOfFunction } from './';
+import { ExchangeRate } from './exchange.js';
+
+$(document).ready(function() {
+  $("form.exchange-select").submit(function(e) {
+    e.preventDefault();
+
+    let usaDollar = $("#user-dollar").val();
+    let newExchange = $("exchange-type").val();
+
+    (async () => {
+      let exchangeSelect = new ExchangeRate();
+      const response = await exchangeSelect.getExchangeRateByCountry(newExchange);
+      getElements(response);
+    })();
+
+    function getElements(response) {
+      if (response) {
+        $("#total").text(`Your total is ${response[8].newExchange}`)
+      } else {
+
+      }
+    }
+  });
+});
